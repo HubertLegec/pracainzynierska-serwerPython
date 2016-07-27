cc7ed08cf77db9278559785ea9b64ead7d5b03cc
HubertLegec <hubert.legec@gmail.com>
2016-07-28 01:50:27 +0200

    bag of visual words

---
+++ src/visual_search_engine/bow.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+class BOW:
+    def __init__(self, vocabulary, matcher, extractor):
+        self.vocabulary = vocabulary
+        self.extractor = extractor
+        self.bowDescriptorExtractor = cv2.BOWImgDescriptorExtractor(self.extractor, matcher)
+        self.bowDescriptorExtractor.setVocabulary(vocabulary)
+
+    def __init__(self, images, cluster_size, matcher, extractor):
+        self.vocabulary = BOW.generate_vocabulary(images, cluster_size, extractor)
+        self.extractor = extractor
+        self.bowDescriptorExtractor = cv2.BOWImgDescriptorExtractor(self.extractor, matcher)
+        self.bowDescriptorExtractor.setVocabulary(self.vocabulary)
+
+    def generate_histogram(self, image):
+        features = self.extractor.detect(image)
+        return self.bowDescriptorExtractor.compute(image, features)[0]
+
+    @staticmethod
+    def generate_vocabulary(images, cluster_size, extractor):
+        bow_trainer = cv2.BOWKMeansTrainer(cluster_size)
+        for img in images:
+            bow_trainer.add(extractor.detectAndCompute(img, None)[1])
+        return bow_trainer.cluster()
