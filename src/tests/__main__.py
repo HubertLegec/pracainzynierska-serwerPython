5966d8b4bfc1c11adbeebd6d75ff0f873ad1a989
HubertLegec <hubert.legec@gmail.com>
2016-10-11 00:29:27 +0200

    ranker provider test

--- src/tests/__main__.py
+++ src/tests/__main__.py
@@ -4,6 +4,7 @@ from tests.rest_test import *
 from tests.config_parsing_test import *
 from tests.matcher_provider_test import *
 from tests.extractor_provider_test import *
+from tests.ranker_provider_test import *
 
 """Run all tests"""
 if __name__ == '__main__':
---
+++ src/tests/ranker_provider_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from tests.utils import get_object_name_with_package
+from visual_search_engine import RankerProvider
+
+
+class RankerProviderTest(unittest.TestCase):
+    def test_get_default_ranker(self):
+        ranker = RankerProvider.get_ranker()
+        ranker_name = get_object_name_with_package(ranker)
+        method = ranker.method
+        self.assertEqual('visual_search_engine.ranker.simple_ranker.SimpleRanker', ranker_name)
+        self.assertEqual('CHI_SQUARED_ALT', method)
+
+    def test_get_tfidf_ranker(self):
+        config = {
+            'mode': 'TFIDF',
+            'method': 'CORRELATION'
+        }
+        ranker = RankerProvider.get_ranker(config)
+        ranker_name = get_object_name_with_package(ranker)
+        method = ranker.method
+        self.assertEqual('visual_search_engine.ranker.tfidf_ranker.TFIDFRanker', ranker_name)
+        self.assertEqual('CORRELATION', method)
+
+if __name__ == '__main__':
+    unittest.main()
